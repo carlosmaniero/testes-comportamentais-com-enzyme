39f2bfe05810f547afd188ffee675229d3505065
Carlos Maniero <carlosmaniero@gmail.com>
2018-06-26 00:10:00 -0300

    Configura webpack para carregar arquivos estáticos

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,63 @@
 const path = require('path');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './src/index.html',
   filename: 'index.html',
   inject: 'body'
+});
+
+const RawExamplesPlugin = new WebpackShellPlugin({
+    onBuildStart:['cp -r ./src/examples/ ./src/.raw']
 })
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    filename: '[name].[chunkhash].bundle.js'
   },
   module: {
     rules: [
-      { test: /\.css$/,
+      { 
+        test: /\.css$/,
         use: [
           { loader: "style-loader" },
           { loader: "css-loader" }
         ]
-      },
+      }, 
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: "babel-loader"
-      }, {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: [/node_modules/, /\.raw/],
         use: "babel-loader"
       }
     ]
   },
-  plugins: [HtmlWebpackPluginConfig],
+  plugins: [RawExamplesPlugin, HtmlWebpackPluginConfig],
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      minChunks: 1,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    }
   }
 }

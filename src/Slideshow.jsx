c41edcab51faa96924e4202f8402eb026c1be978
Carlos Maniero <carlosmaniero@gmail.com>
2018-06-27 00:50:47 -0300

    Adiciona exemplo usando find

--- src/Slideshow.jsx
+++ src/Slideshow.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import mainBg from '../assets/mainBg.png';
 import createTheme from 'spectacle/lib/themes/default';
+import CodeSlide from 'spectacle-code-slide';
+
 import { Heading } from './deck/Heading';
 
-import olaTestExample from 'raw-loader!./.raw/examples/mount/Ola.test.js';
-import olaExample from 'raw-loader!./.raw/examples/mount/Ola.js';
+import { Ola as OlaMount } from './examples/mount/Ola';
+import { Ola as OlaFind } from './examples/find/Ola';
+import mountTestExample from 'raw-loader!./.raw/examples/mount/Ola.test.js';
+import mountExample from 'raw-loader!./.raw/examples/mount/Ola.js';
+import findTestExample from 'raw-loader!./.raw/examples/find/Ola.test.js';
 
 import {
     Appear,
@@ -24,8 +29,7 @@ import {
     Text
 } from 'spectacle';
 
-import CodeSlide from 'spectacle-code-slide';
-import { Ola } from './examples/mount/Ola';
+
 
 const textColor = '#333';
 
@@ -58,14 +62,16 @@ export default class Slideshow extends React.Component {
             </Slide>
             <Slide>
                 <Heading textSize="4rem" margin={50}>Primeiro teste</Heading>
+                <Appear>
                 <Text padding={20}>
                     Conhecendo o <Code>mount()</Code>.
                 </Text>
+                </Appear>
             </Slide>
             <CodeSlide
                 lang="js"
                 bgColor="black"
-                code={olaTestExample}
+                code={mountTestExample}
                 ranges={[
                     { loc: [0, 13] },
                     { loc: [1, 3] },
@@ -76,21 +82,40 @@ export default class Slideshow extends React.Component {
                 ]} />
             <Slide>
                 <Heading textSize="4rem" margin={50}>Fazendo o teste passar</Heading>
+
+                <Appear>
+                    <Text>Afinal, a gente usa <Code>TDD</Code>, né?</Text>
+                </Appear>
             </Slide>
             <CodeSlide
                 lang="js"
                 bgColor="black"
-                code={olaExample}
-                ranges={[{ loc: [0, 7] }]}
+                code={mountExample}
+                ranges={[{ loc: [0, 9] }]}
                 />
             <Slide>
-                <Heading textSize="4rem" margin={50}>Resultado Final:</Heading>
-                
-                <Ola name="Mundo" />
-                
+                <Heading textSize="4rem" margin={50}>Resultado:</Heading>
+                <OlaMount name="Mundo" />
                 <Appear>
                     <Text>Sim! O código acima é o nosso component :)</Text>
                 </Appear>
+            </Slide>
+            <Slide>
+                <Heading textSize="4rem" margin={50}>Buscando por elementos</Heading>
+            </Slide>
+            <CodeSlide
+                lang="js"
+                bgColor="black"
+                code={findTestExample}
+                ranges={[
+                    { loc: [6, 12] },
+                    { loc: [9, 11] },
+                    { loc: [16, 18] },
+                    { loc: [9, 11] }
+                ]} />
+            <Slide>
+                <Heading textSize="4rem" margin={50}>Resultado:</Heading>
+                <OlaFind name="Mundo" />
             </Slide>
         </Deck>
       );
---
+++ src/examples/find/Ola.test.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import { Ola } from './Ola';
+
+
+describe('Dizendo Olá', () => {
+    it('mostra o nome como bold', () => {
+        const wrapper = mount(
+            <Ola name="Mundo" />);
+        expect(wrapper.find('strong').text())
+            .toBe("Mundo");
+    });
+
+    it('diz Olá para o nome', () => {
+        const wrapper = mount(
+            <Ola name="Mundo" />);
+        expect(wrapper.text())
+            .toBe("Olá, Mundo!");
+    });
+});
